e388ab5048665df96c1e95d0f56369d7308fd1ef
Zach Cossman <Zachcos@gmail.com>
2022-08-01 17:28:35 -0400

    Add variables file

---
+++ data/variables.ts
@@ -0,0 +1,32 @@
+export const color = {
+  dark: '#222834',
+  med: '#38596b',
+  light: '#9eb0bb',
+  accent: '#52514e',
+};
+
+export const font = {
+  display: '4.768rem',
+  heading: '1.953rem',
+  subheading: '1.25rem',
+  subsubheading: '0.8rem',
+  body: '1rem',
+  medium: 600,
+  bold: 800,
+};
+
+const size = {
+  SMphone: '425px',
+  phone: '600px',
+  tablet: '1000px',
+  desktop: '1366px',
+  LGdesktop: '1920px',
+};
+
+export const device = {
+  SMphone: `(max-width: ${size.SMphone})`,
+  phone: `(max-width: ${size.phone})`,
+  tablet: `(max-width: ${size.tablet})`,
+  desktop: `(max-width: ${size.desktop})`,
+  LGdesktop: `(max-width: ${size.LGdesktop})`,
+};
