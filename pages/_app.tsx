b68652d54aa3d150cc80143d05a1a733aa889561
Zach Cossman <Zachcos@gmail.com>
2022-08-02 09:00:03 -0400

    Import Nav component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globalStyles';
 import GridWrapper from '../components/gridWrapper';
+import Nav from '../components/nav';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
       <GridWrapper>
+        <Nav />
         <Component {...pageProps} />
       </GridWrapper>
     </>
