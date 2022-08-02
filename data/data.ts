567d1fb9300069fa043fe56ec39b996ba8be9ca0
Zach Cossman <Zachcos@gmail.com>
2022-08-02 11:46:27 -0400

    Create data.ts file

---
+++ data/data.ts
@@ -0,0 +1,43 @@
+const videoData: VideoDataProps[] = [
+  {
+    title: 'Georgia on my Mind',
+    artist: 'Ray Charles',
+    videoUrl: 'https://youtu.be/L_dQycqw-aA',
+    findMe: 'georgiaOnMyMind',
+    thumbUrl: '/assets/georgiaOnMyMind.png',
+    index: 1,
+  },
+  {
+    title: 'Feathered Indians',
+    artist: 'Tyler Childers',
+    videoUrl: 'https://youtu.be/_XK7NsJIB-s',
+    findMe: 'featheredIndians',
+    thumbUrl: '/assets/featheredIndians.png',
+    index: 2,
+  },
+  {
+    title: 'Where Rainbows Never Die',
+    artist: 'The Steeldrivers',
+    videoUrl: 'https://youtu.be/lUA--zWVwZ4',
+    findMe: 'whereRainbowsNeverDie',
+    thumbUrl: '/assets/whereRainbowsNeverDie.png',
+    index: 3,
+  },
+  {
+    title: 'If You Were the Rain',
+    artist: 'Stephen Day',
+    videoUrl: 'https://youtu.be/zsMcn5ukpok',
+    findMe: 'ifYouWereTheRain',
+    thumbUrl: '/assets/ifYouWereTheRain.png',
+    index: 4,
+  },
+];
+
+const headshotsData: headshotDataProps[] = [
+  { path: '/assets/headshot_1jpg', index: 1 },
+  { path: '/assets/headshot_2jpg', index: 2 },
+  { path: '/assets/headshot_3jpg', index: 3 },
+  { path: '/assets/headshot_4jpg', index: 4 },
+];
+
+export { videoData, headshotsData };
