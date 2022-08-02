1dece49f7709ab2e9281df8d62aa57cd922fef4a
Zach Cossman <Zachcos@gmail.com>
2022-08-02 09:06:25 -0400

    Create HeroText component

---
+++ components/heroText.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color, font, device } from '../data/variables';
+
+const NameBox = styled.div`
+  align-items: center;
+  background: ${color.med};
+  display: flex;
+  justify-content: center;
+  .container {
+    overflow: hidden;
+    position: relative;
+    z-index: 10;
+    h1 {
+      font-size: ${font.display};
+      font-weight: ${font.bold};
+      text-transform: uppercase;
+      @media ${device.phone} {
+        font-size: 3rem;
+      }
+      @media ${device.SMphone} {
+        font-size: 2.4rem;
+      }
+    }
+  }
+`;
+
+interface Props {
+  area: string;
+  text: string;
+}
+
+const HeroText = ({ area, text }: Props) => {
+  return (
+    <NameBox style={{ gridArea: area }}>
+      <div className='container'>
+        <h1>{text}</h1>
+      </div>
+    </NameBox>
+  );
+};
+
+export default HeroText;
