f75828829b820d643b0f0562b04aa081469b9a0c
Zach Cossman <Zachcos@gmail.com>
2022-08-02 11:48:34 -0400

    Remove testing console.log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log('this is the vh value: ', vh);
   });
   return (
     <>
